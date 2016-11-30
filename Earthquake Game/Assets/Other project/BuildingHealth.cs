861e43ba00c4d06805343a7c24305bae7827dde1
Andrea Donati <donati.andrea.00@gmail.com>
2016-11-30 08:54:59 +0100

    Fix commit

---
+++ Earthquake Game/Assets/Other project/BuildingHealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BuildingHealth : MonoBehaviour {
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = startingHealth;
+        healthSlider.value = currentHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void takeDamage(int amount)
+    {
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+
+    }
+}
