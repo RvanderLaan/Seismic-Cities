f868fb0179cfe725d22872123edcbe46bb7ddaf9
RvanderLaan <rrm-remi@hotmail.com>
2016-12-05 13:32:35 +0100

    Improved look 'n feel of level selector, added the Netherlands

--- Earthquake Game/Assets/Scripts/Menu/CountrySelection.cs
+++ Earthquake Game/Assets/Scripts/Menu/CountrySelection.cs
@@ -7,14 +7,28 @@ public class CountrySelection : MonoBehaviour {
     public Canvas hoverDialog;
     public Canvas levelDetails;
 
+
+    private Renderer renderer;
+
+    void Start() {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update() {
+        hoverDialog.transform.rotation = Quaternion.identity;
+    }
+
     private void OnMouseEnter()
     {
-        hoverDialog.gameObject.SetActive(true);
+        // hoverDialog.gameObject.SetActive(true);
+        renderer.material.color = Color.green;
+
     }
 
     private void OnMouseExit()
     {
-        hoverDialog.gameObject.SetActive(false);
+        // hoverDialog.gameObject.SetActive(false);
+        renderer.material.color = Color.yellow;
     }
 
     private void OnMouseDown()
