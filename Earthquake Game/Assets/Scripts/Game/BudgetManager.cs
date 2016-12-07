580d3d5c6797009e1f2e7494a780c8dbc7ad4f1f
Andrea Donati <donati.andrea.00@gmail.com>
2016-12-07 11:15:05 +0100

    Implemented the money and population manager. Added the computation of the score based on the damage taken by the buildings and the money left

---
+++ Earthquake Game/Assets/Scripts/Game/BudgetManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BudgetManager : MonoBehaviour {
+
+    public int startingMoney;
+    public int targetPopulation;
+
+    public Text moneyText;
+    public Text populationText;
+    public Text notEnoughMoneyText;
+
+    public Color redColor;
+
+    private int actualMoney;
+    private int actualPopulation;
+
+	// Use this for initialization
+	void Start () {
+        actualMoney = startingMoney;
+        actualPopulation = 0;
+        updateGUI();
+
+        notEnoughMoneyText.canvasRenderer.SetAlpha(0);
+    }
+
+    public void newBuilding(int cost, int people)
+    {
+        if (enoughMoney(cost))
+        {
+            actualMoney -= cost;
+            actualPopulation += people;
+            updateGUI();
+        }
+    }
+
+    public bool enoughMoney(int cost)
+    {
+        return actualMoney >= cost;
+    }
+
+    public bool enoughPopulation()
+    {
+        return actualPopulation >= targetPopulation;
+    }
+
+    private void updateGUI()
+    {
+        moneyText.text = "Money = " + actualMoney + "/" + startingMoney;
+        populationText.text = "Population = " + actualPopulation + "/" + targetPopulation;
+    }
+
+    public void notEnoughMoneyMessage()
+    {
+        //Debug.Log("notEnoughMoneyMessage()");
+        notEnoughMoneyText.canvasRenderer.SetAlpha(1);
+        notEnoughMoneyText.CrossFadeAlpha(0, 2, true);
+    }
+
+    public int getLeftMoney()
+    {
+        return actualMoney;
+    }
+}
---
+++ Earthquake Game/Assets/Scripts/Game/BuildingInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingInfo : MonoBehaviour {
+
+    public int cost = 100;
+    public int population = 20;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+}
