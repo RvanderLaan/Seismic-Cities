98f8b1dbe6ec5e1615e9f97defc9c83fb68a52b5
Andrea Donati <donati.andrea.00@gmail.com>
2017-01-11 18:13:01 +0100

    Added the dialogs in the test level at the beginning of the level, when the player wins and when he fails.

---
+++ Earthquake Game/Assets/Scripts/Menu/DialogItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class DialogItem
+{
+    public CharacterName characterName;
+    public string dialogText;
+    public UnityEvent enterEvent;
+    public UnityEvent leaveEvent;
+
+    public DialogItem()
+    {
+
+    }
+
+    public enum CharacterName
+    {
+        Poseidon, Aphrodite
+    }
+}
