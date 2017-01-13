ea4af8d0dc43b519b8b3f6f783643c95d919e9b7
Andrea Donati <donati.andrea.00@gmail.com>
2017-01-13 20:02:59 +0100

    Blocked the clicks outside the dialog boxes. Blocked all the clicks in the tutorial except the OK buttons. Added the skip buttons for both dialogs and tutorials. Added the menu panel where the player can restart the level or go back to the level selection menu.

---
+++ Earthquake Game/Assets/Scripts/Menu/MenuPanel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPanel : MonoBehaviour {
+
+	public void show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
