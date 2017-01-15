00c683de7c91f369e99e30be9011d22d4fb887dd
Andrea Donati <donati.andrea.00@gmail.com>
2017-01-15 10:58:24 +0100

    Now the camera moves through the tutorial, since the player cannot perform any action. Some minor improvements.

--- Earthquake Game/Assets/Scripts/Menu/TutorialManager.cs
+++ Earthquake Game/Assets/Scripts/Menu/TutorialManager.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour {
 
+    public Transform cameraContainer;
     public GameObject background;
     public GameObject skipButton;
     public List<GameObject> tutorialInstructions;
 
+    
+
     private int currentInstruction = 0;
 
+    public void Start()
+    {
+    }
 	
     public void startTutorial()
     {
         background.SetActive(true);
         skipButton.SetActive(true);
         currentInstruction = 0;
-        tutorialInstructions[currentInstruction++].SetActive(true);
+        showInstruction(tutorialInstructions[currentInstruction++]);
     }
 
     public void nextInstruction()
@@ -24,7 +30,7 @@ public class TutorialManager : MonoBehaviour {
         if (currentInstruction < tutorialInstructions.Count && currentInstruction > 0)
         {
             tutorialInstructions[currentInstruction - 1].SetActive(false);
-            tutorialInstructions[currentInstruction++].SetActive(true);
+            showInstruction(tutorialInstructions[currentInstruction++]);
         }
         else
         {
@@ -44,5 +50,23 @@ public class TutorialManager : MonoBehaviour {
     {
         background.SetActive(false);
         skipButton.SetActive(false);
+    }
+
+    private void showInstruction(GameObject instruction)
+    {
+        instruction.SetActive(true);
+        WorldToScreenMap script = instruction.GetComponent<WorldToScreenMap>();
+
+        if (script != null)
+        {
+            
+            //camera.gameObject.GetComponentInParent<CamMovement>().enabled = false;
+            
+            cameraContainer.position = new Vector3(script.follow.transform.position.x,
+                                                    script.follow.transform.position.y,
+                                                    cameraContainer.position.z);
+                                                    
+            
+        }
     }
 }
