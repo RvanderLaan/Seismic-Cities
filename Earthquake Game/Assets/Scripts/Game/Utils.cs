d19aaf3edcf333428273312129828f472acc8cec
Andrea Donati <donati.andrea.00@gmail.com>
2017-01-16 15:44:20 +0100

    Added dark background to dialogues and tutorial. Now the restart and next level buttons appear after the last dialogue at the end of the level.

--- Earthquake Game/Assets/Scripts/Game/Utils.cs
+++ Earthquake Game/Assets/Scripts/Game/Utils.cs
@@ -13,4 +13,10 @@ public class Utils : MonoBehaviour {
     public void LoadScene(string name) {
         SceneManager.LoadScene(name);
     }
+
+    public void activateGameObject(GameObject go)
+    {
+        Debug.Log("aaaa");
+        go.SetActive(true);
+    }
 }
