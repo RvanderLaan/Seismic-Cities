3fbdde8539421c425f459ee001a0d9d58afe90c8
Remi <rrm-remi@hotmail.com>
2017-05-17 23:11:08 +0200

    Started editor + grid

---
+++ Earthquake Game/Assets/Editor/SoilTerrainEditor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Grid))]
+public class SoilTerrainEditor : Editor {
+
+    Grid grid;
+
+    Texture[] shapes;
+
+    int shape = 0;
+    int soil = 0;
+    Material mat;
+
+    void OnEnable()
+    {
+        grid = (Grid) target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        // base.OnInspectorGUI();
+        serializedObject.Update();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Grid width");
+        EditorGUILayout.Slider(1, 1f, 100f, null);
+        GUILayout.EndHorizontal();
+
+
+        GUIContent[] content = new GUIContent[8];
+        for (int i = 0; i < content.Length; i++)
+        {
+            content[i] = new GUIContent("Shape " + i);
+        }
+
+        shape = GUILayout.SelectionGrid(shape, content, 4);
+
+        GUIContent[] content2 = new GUIContent[8];
+        for (int i = 0; i < content.Length; i++)
+        {
+            content2[i] = new GUIContent("Soil " + i);
+        }
+
+        soil = GUILayout.SelectionGrid(soil, content2, 4);
+
+
+        mat = (Material) EditorGUILayout.ObjectField(mat, typeof(Material));
+
+
+    }
+
+}
---
+++ Earthquake Game/Assets/Grid.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class Grid : MonoBehaviour {
+
+    public float size = 32f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+
+        
+        Vector3 pos = Camera.current.transform.position;
+        Gizmos.color = Color.green;
+
+        Rect viewport = Camera.current.pixelRect;
+
+        Rect rect = new Rect(-8 * size, -16 * size, 32 * size, 18 * size);
+        
+        for (float i = rect.yMin; i <= rect.yMax; i += size)
+        {
+            float y = Mathf.Floor(i / size) * size;
+            Gizmos.DrawLine(new Vector3(rect.xMin, y, 0), new Vector3(rect.xMax, y, 0)); 
+        }
+
+        for (float i = rect.xMin; i <= rect.xMax; i += size)
+        {
+            float y = Mathf.Floor(i / size) * size;
+            Gizmos.DrawLine(new Vector3(y, rect.yMin, 0), new Vector3(y, rect.yMax, 0));
+        }
+
+    }
+}
